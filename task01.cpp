cd0e0aefe4c3ce9c496741de0658fda540aba9c5
Christy Arys <carys@student.21-school.ru>
2022-11-22 11:37:20 +0300

    task01 &task02

---
+++ task01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "C++ is a general-purpose programming language with a bias towards systems programming that\n\n";
+    std::cout <<"  - is a better C\n";
+    std::cout <<"  - supports data abstraction\n";
+    std::cout <<"  - supports object-oriented programming\n";
+    std::cout <<"  - supports generic programming.\n";
+}
